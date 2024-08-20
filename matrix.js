fb47f1b45fddd67c347a2be71239323082687f50
Fayas Muhammed Hashim <97302873+fayasmh07@users.noreply.github.com>
2024-08-20 20:44:09 +0530

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -28,7 +28,7 @@ function updateMatrix() {
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
   // Set the text color and font
-  ctx.fillStyle = "green";
+  ctx.fillStyle = "#CB0000";
   ctx.font = "12px timesnewroman";
 
   // Loop through each column
